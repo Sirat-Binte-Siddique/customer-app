332390ab424cc303597d145da8fb36556dc9644b
shoshe_siddique <iubat123shoshe@gmail.com>
2018-07-18 15:56:12 +0600

    trying to submit button

--- src/app/customers/customer/customer.component.ts
+++ src/app/customers/customer/customer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 import { CustomerService } from '../service/customer.service';
+import { Customer } from '../service/customer.model'
 
 @Component({
   selector: 'app-customer',
@@ -20,7 +21,6 @@ export class CustomerComponent implements OnInit {
   resetForm (form? : NgForm){
       if (form != null)
       form.reset();
-      console.log("reset button working");
     this.customerService.selectedCustomer = {
         FirstName : '',
         LastName : '',
@@ -30,13 +30,30 @@ export class CustomerComponent implements OnInit {
     }
   }
 
+    // onSubmit(form : NgForm){
+    //     console.log("submit button working");
+    //     this.customerService.postCustomer(form.value)
+    //     .subscribe(data => {
+    //         this.resetForm(form);
+    //     })
+    // }
 
-    onSubmit(form : NgForm){
-        console.log("submit button working");
-        this.customerService.postCustomer(form.value)
-        .subscribe(data => {
+
+    onSubmit(form: NgForm) {
+        let newCustomer;
+        this.customerService.postCustomer(newCustomer).subscribe(
+          data => {
             this.resetForm(form);
-        })
+            return true;
+          },
+          error => {
+                console.error("Error saving food!");
+                console.log(" somethin is wrong ");
+                // return Observable.throw(error);
+            }
+          );
     }
+
+
 
 }
--- src/app/customers/service/customer.service.ts
+++ src/app/customers/service/customer.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@angular/core';
-
 import { Http, Headers, RequestOptions } from '@angular/http';
-
 // import { Observable } from 'rxjs/Observable';
 // import 'rxjs/add/operator/map';
 // import 'rxjs/add/operator/toPromise';
-
 import { Customer } from './customer.model';
+
 
 @Injectable({
   providedIn: 'root'
@@ -16,21 +14,23 @@ export class CustomerService {
   selectedCustomer : Customer;
   constructor( private http : Http ) {}
 
+    // postCustomer(cus: Customer) {
+    //     // var body = JSON.stringify(cus);
+    //     // var headerOptions = new Headers({ 'Content-Type' : 'application/json' });
+    //     // var requestOptions = new RequestOptions({method : RequestMethod.Post, headers: headerOptions});
+    //     // return this.http.post('http://localhost:4200/api/customers', body, requestOptions).map(x => x.json());
+
+    //     // headers.append('Authorization', 'Bearer' + this.getToken()); // this will need for token authintication but not now
+    // }
+
 
     postCustomer(cus: Customer) {
-        // var body = JSON.stringify(cus);
-        // var headerOptions = new Headers({ 'Content-Type' : 'application/json' });
-        // var requestOptions = new RequestOptions({method : RequestMethod.Post, headers: headerOptions});
-        // return this.http.post('http://localhost:4200/api/customers', body, requestOptions).map(x => x.json());
-
-
-
         const headers = new Headers();
-        // headers.append('Authorization', 'Bearer' + this.getToken()); // this will need for token authintication but not now
         const options = new RequestOptions({ headers: headers });
-        const url = 'http://localhost:4200/postcustomers';
+        const url = 'http://localhost:4200/posts';
         return this.http.post('url', cus, options);
     }
+
 
 
 }
